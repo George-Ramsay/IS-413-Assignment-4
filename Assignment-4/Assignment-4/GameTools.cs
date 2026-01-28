e3d3a2570c4e3c398fafb24d50a4ae3fc510a44b
Nathan Trotter <nathanrtrotter@gmail.com>
2026-01-28 14:47:51 -0700

    Completed Gameboard print method

--- Assignment-4/Assignment-4/GameTools.cs
+++ Assignment-4/Assignment-4/GameTools.cs
@@ -3,12 +3,30 @@ namespace Assignment_4;
 public class GameTools
 {
     private int[] moves = int[9];
+
     // Prints the current state of the game board
     public void PrintBoard(char[] board)
     {
-        
+        for (int i = 0; i < 3; i++)
+        {
+            Console.Write("\n");
+            for (int j = 0; i < 3; i++)
+            {
+                if (board[i] == 2)
+                {
+                    Console.Write(board[i]);
+                }
+                else
+                {
+                    Console.Write(board[i]);
+                    Console.Write("|");
+                }
+
+            }
+        }
     }
-    // Handles user input and ensures a valid, unoccupied position is selected
+    
+// Handles user input and ensures a valid, unoccupied position is selected
     public int GetValidatedMove(char[] board, int position);
 
     // Applies a validated move to the game board
@@ -21,5 +39,5 @@ public class GameTools
     public bool IsBoardFull(char[] board);
     
     // Converts a linear board position (0–8) into row and column indices
-    private (int row, int col) ConvertPosition(int position);
+   // private (int row, int col) ConvertPosition(int position);
 }
