db27421a28b8f65a0404bb3dbb601b68b37e376d
Nathan Trotter <nathanrtrotter@gmail.com>
2026-01-28 14:25:39 -0700

    Removed 2D array argument from methods

--- Assignment-4/Assignment-4/GameTools.cs
+++ Assignment-4/Assignment-4/GameTools.cs
@@ -2,23 +2,23 @@ namespace Assignment_4;
 
 public class GameTools
 {
+    private int[] moves = int[9];
     // Prints the current state of the game board
-    public void PrintBoard(char[,] board)
+    public void PrintBoard(char[] board)
     {
         
     }
-    
     // Handles user input and ensures a valid, unoccupied position is selected
-    public int GetValidatedMove(char[,] board, int position);
+    public int GetValidatedMove(char[] board, int position);
 
     // Applies a validated move to the game board
-    public void ApplyMove(char[,] board, int position, char playerMark);
+    public void ApplyMove(char[] board, int position, char playerMark);
 
     // Checks whether a winning condition has been met
-    public bool TryGetWinner(char[,] board, out char winnerMark);
+    public bool TryGetWinner(char[] board, out char winnerMark);
 
     // Determines whether the board is full
-    public bool IsBoardFull(char[,] board);
+    public bool IsBoardFull(char[] board);
     
     // Converts a linear board position (0–8) into row and column indices
     private (int row, int col) ConvertPosition(int position);
