efd5bfe4dc6a9df882e8bd19ad38c9a6010ed8e4
Nathan Trotter <nathanrtrotter@gmail.com>
2026-01-28 14:22:00 -0700

    Argument change to the GetValidatedMove method

--- Assignment-4/Assignment-4/GameTools.cs
+++ Assignment-4/Assignment-4/GameTools.cs
@@ -3,10 +3,13 @@ namespace Assignment_4;
 public class GameTools
 {
     // Prints the current state of the game board
-    public void PrintBoard(char[,] board);
-
+    public void PrintBoard(char[,] board)
+    {
+        
+    }
+    
     // Handles user input and ensures a valid, unoccupied position is selected
-    public int GetValidatedMove(char[,] board, char currentPlayer);
+    public int GetValidatedMove(char[,] board, int position);
 
     // Applies a validated move to the game board
     public void ApplyMove(char[,] board, int position, char playerMark);
