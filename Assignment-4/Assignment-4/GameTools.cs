d770f0903f930f7db0225fd177a6d3fb47ea6b81
Gunnjake <Gunnjakeim@gmail.com>
2026-01-28 19:11:43 -0700

    fixed some of the logic

--- Assignment-4/Assignment-4/GameTools.cs
+++ Assignment-4/Assignment-4/GameTools.cs
@@ -10,31 +10,21 @@ public class GameTools
         for (int i = 0; i < 3; i++)
         {
             Console.Write("\n");
-            for (int j = 0; i < 3; i++) //Needs to be j not i
+            for (int j = 0; j < 3; j++)
             {
-                // int index = i * 3 + j; This will get us to the right index
-                // Console.Write(board[index]);
-                // if (j < 2) Console.Write("|");
+                int index = i * 3 + j;     // converts row/col 
+                Console.Write(board[index]);
 
-                if (board[i] == 2)                                        
-                {
-                    Console.Write(board[i]);   
-                }
-                else
-                {
-                    Console.Write(board[i]);
-                    Console.Write("|");
-                }
-
-            }         
-            // Console.WriteLine();
+                if (j < 2) Console.Write("|"); 
+            }
+            Console.WriteLine();
         }
     }
     
     // Handles user input and ensures a valid, unoccupied position is selected
     public bool GetValidatedMove(char[] board, int position)
     { 
-        // if (position < 0 || position > 8) return false; Check if the position is within valid range
+        if (position < 0 || position > 8) return false; 
         return board[position] == '-';
     }
     
